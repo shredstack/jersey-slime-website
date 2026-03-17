85fe40caec3d1b64b2ded235c954bab205e8cd30
Sarah Dorich <sarah.dorich.biz@gmail.com>
2026-03-17 09:53:38 -0600

    fix middleware

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -10,16 +8,6 @@ const nextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
     ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ensure __dirname is defined in serverless bundles on Vercel
-      config.node = {
-        ...config.node,
-        __dirname: true,
-      }
-    }
-    return config
   },
 }
 
